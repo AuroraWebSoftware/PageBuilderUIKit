d2fdbd8141854774ad03da4ae774ddbeb713906f
Nurullah Sevinçtekin <nsevinctekin@gmail.com>
2022-10-20 13:55:08 +0300

    Showcase component'inde resim alanının slider olarak boyutlandırılması problemi çözüldü.

--- view/base/web/js/content-type/showcase/appearance/carousel/widget.js
+++ view/base/web/js/content-type/showcase/appearance/carousel/widget.js
@@ -40,7 +40,7 @@ define([
      */
     function initSlider($element, slickConfig, breakpoint) {
         var productCount = $element.find('.product-item').length,
-            $carouselElement = $($element.children().children()),
+            $carouselElement = $($element.find(".widget-product-carousel")),
             centerModeClass = 'center-mode',
             carouselMode = $element.data('carousel-mode'),
             slidesToShow = breakpoint.options.products[carouselMode] ?
